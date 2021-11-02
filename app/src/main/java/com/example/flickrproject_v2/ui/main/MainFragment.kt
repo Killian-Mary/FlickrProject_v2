71245d1b953702f56365d33240883bfd12304bae
Killian Mary <killian.mary@etudiant.univ-lr.fr>
2021-11-02 15:09:44 +0100

    Adding the photo on the Main Page of the app + Next image button works

--- app/src/main/java/com/example/flickrproject_v2/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/flickrproject_v2/ui/main/MainFragment.kt
@@ -1,12 +1,20 @@
 package com.example.flickrproject_v2.ui.main
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.example.flickrproject_v2.R
+import com.example.flickrproject_v2.R.layout.main_fragment
+import com.example.flickrproject_v2.model.Photo
 
 class MainFragment : Fragment() {
 
@@ -20,7 +28,28 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        var url: String = ""
+        val layout =  inflater.inflate(main_fragment, container, false)
+        val nextButton = layout.findViewById<Button>(R.id.btn_next)
+        val allButton = layout.findViewById<Button>(R.id.btn_allImages)
+        val imageTitle = layout.findViewById<TextView>(R.id.tv_title)
+        val imageView = layout.findViewById<ImageView>(R.id.iv_central)
+
+        // Observer
+        val observer = Observer<Photo> { photo ->
+            val url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id+"_"+photo.secret + ".jpg"
+            imageTitle.text = photo.title
+            Glide.with(layout).load(url).into(imageView)
+        }
+        viewModel.photo.observe(viewLifecycleOwner, observer)
+
+        // Next image
+        nextButton.setOnClickListener {
+            viewModel.nextPhoto()
+        }
+        return layout
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/flickrproject_v2/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/flickrproject_v2/ui/main/MainViewModel.kt
@@ -1,7 +1,42 @@
 package com.example.flickrproject_v2.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flickrproject_v2.model.Photo
+import com.example.flickrproject_v2.repository.FlickrAPI
+import com.example.flickrproject_v2.repository.Repository
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var photo = MutableLiveData<Photo>()
+    var photoList = ArrayList<Photo>();
+
+    init {
+        viewModelScope.launch {
+
+            val repository = Repository();
+            val photosResult = repository.getPhotos().photos;
+            for (photo in photosResult.photo) {
+                photoList.add(photo)
+            }
+            photo.value = photosResult.photo[0]
+        }
+    }
+
+    fun nextPhoto() {
+        val currentVal = photo.value
+        var nextIndex: Int
+        if(currentVal === null) {
+            nextIndex = 0
+        } else {
+            val currentIndex = photoList.indexOf(currentVal)
+            nextIndex = currentIndex + 1
+            if(currentIndex === photoList.size - 1) {
+                nextIndex = 0
+            }
+        }
+        photo.value = photoList[nextIndex]
+    }
 }
